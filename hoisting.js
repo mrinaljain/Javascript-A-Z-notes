508f979c07c0302ea6d7170bdb0b8f3be841cf6a
mrinaljain <jain.mrinal140@gmail.com>
2024-03-14 02:53:43 +0530

    fresh code

---
+++ hoisting.js
@@ -0,0 +1,32 @@
+// var a = 10;
+
+// function call() {
+//    console.log(a);
+//    let a = 20;
+// }
+
+// call();
+
+// var y = 1,
+//    x = y = typeof x
+// console.log(x);
+
+// var b = 1;
+
+// function outer() {
+//    var b = 2
+//    function inner() {
+//       var b;
+//       b++;
+//       var b = 3;
+//       console.log(b)
+//    }
+//    inner();
+// }
+
+// outer();
+
+
+var x = 3;
+var y = x++;
+y += 1;
