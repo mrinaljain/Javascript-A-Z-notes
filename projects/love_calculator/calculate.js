2b5fa08842efc42a33b4c0e3287ff0f7e4701a43
mrinaljain <jain.mrinal140@gmail.com>
2024-03-18 05:41:56 +0530

    Function Borrowing  Examples added

--- projects/love_calculator/calculate.js
+++ projects/love_calculator/calculate.js
@@ -8,12 +8,22 @@ setTimeout(() => {
 
 
 function calCulateLovePercentage(nameOne , nameTwo) {
+
+  let lovePercent = calculateRandomPercentage();
+
+  if (lovePercent > 70 && lovePercent < 100 ) {
+    alert('Thats awesome ' + nameOne + ' and ' + nameTwo + ' is ' + lovePercent + '%');
+    
+  }else{
+    alert( 'Love Percentage of ' + nameOne + ' and ' + nameTwo + ' is ' + lovePercent + ' %');
+
+  }
    
-  alert( 'Love Percentage of' + nameOne + 'and' + nameTwo + 'is' + calculateRandomPercentage());
 }
 
  function calculateRandomPercentage() {
    let randomPercent = 0;
    randomPercent = Math.floor(Math.random() * 100);
-    return randomPercent;
+   // +1 added to shift range from 0-99 to 1 -100
+    return randomPercent +1;
 }
