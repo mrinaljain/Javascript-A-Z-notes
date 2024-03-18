462b71c6b42285a874411e6a70eb3b3d7b760ea5
mrinaljain <jain.mrinal140@gmail.com>
2024-03-18 06:41:24 +0530

    Diffrent cases of this keyword

--- this.js
+++ this.js
@@ -1,13 +1,21 @@
 'use strict';
-
-// this in the global space
+// console.error('STRICT MODE ON');
+//! this in the global space
+/// this in global space points to the globalObject of the Javascript Rntime Environment 
+/// i.e  [windows] in case of chrome browser
 console.log('Global Space');
 console.log(this);
 
 
-// this inside function 
+//! this inside function 
+//! this in strict mode
+/// this inside a function depends on the mode [strict , non strict] of the javascript in which its running
+/// strict mode ON  -->  undefined
+/// strict mode OFF -->  window
+/// On top of strict mode the value of this also depends on the object reffrence through which  the function invocation happens
+/// window.fun();  --> wndow
 
-function a(){
+function a() {
    // let b = 10;
    console.log(this);
 }
@@ -15,3 +23,57 @@ console.log('Inside Function Without object refrence');
 a();
 console.log('Inside Function With object refrence');
 window.a();
+
+
+//! this inside object's method
+/// this inside object's method [method should not be an arrow function] points to the object itself
+let animal = {
+   breedName: 'ABC',
+   age: 26,
+   sound: 'woff',
+   makeSound: function () {
+      console.log(this);
+   }
+}
+console.log('this inside object\'s method[method should not be an arrow function]points to the object itself');
+animal.makeSound();
+
+
+//! this inside arrow function
+/// this inside the arrow function points to the parent\s this reffrence
+console.log('this inside the arrow function points to the parent\s this reffrence');
+(() => console.log(this))();
+
+
+//! this inside arrow function inside object
+let anotherAnimal = {
+   breedName: 'ABC',
+   age: 26,
+   sound: 'woff',
+   makeSound: () => console.log(this)
+}
+console.log('this inside the Objects\'s arrow function points to the parent\s this reffrence');
+anotherAnimal.makeSound();
+
+//! this inside nested arrow function
+
+let yetAnotherAnimal = {
+   breedName: 'ABC',
+   age: 26,
+   sound: 'woff',
+   makeSound: function() {
+      (() => console.log(this))();
+   }
+}
+console.log('this inside the Objects\'s NESTED arrow function points to the parent\s this reffrence');
+yetAnotherAnimal.makeSound();
+
+// this inside DOM
+
+// diffrence btwn Method / function
+
+// this keyword inside class 
+
+// this keyword inside constructors
+
+// call apply bind methods
