403ccf4cf2efa2fd5705fa4938dcc2e557664953
mrinaljain <jain.mrinal140@gmail.com>
2024-03-27 21:39:16 +0530

    throttle debounce

--- debounce_throttle/debounce.js
+++ debounce_throttle/debounce.js
@@ -5,43 +5,37 @@
 // 3) once timer complete make API call 
 // 4) if the typing event comes before timer ends then restart the timer. 
 
-let searchbtn = document.getElementById('searchBox');
+let searchBox = document.getElementById('searchBox');
 
+///? With Debounce
+searchBox.addEventListener('keyup', betterApiCaller(apiCaller, 1000));
 
-searchbtn.addEventListener('keydown', debounce(apiCaller, 200));
+///? Without Debounce
+// searchBox.addEventListener("keyup", apiCaller);
+
 
 /// Enhanced  function for our API call which will return a callback hence we call it  the debounce function
-function debounce(fun, debounceTime) {
-   let timerId;
-   return function (e) {
-      clearTimeout(timerId);
-      timerId = setTimeout(fun(e), debounceTime);
-   }
-}
-const debounceFunc = debounce(()=> {
-   console.log("Debounced Function Called");
-}, 300);
-
-
-//! Debounce Enhanced version
-function enhancedDebounce(apiCaller, debounceTime) {
+function betterApiCaller(actualFunctionTobeCalled, delay) {
+   let timer;
    return function () {
-      let timerId;
-      clearTimeout(timerId);
-      timerId = setTimeout(apiCaller, debounceTime);
+      let context = this;
+      args = arguments;
+      if (timer)clearTimeout(timer);
+      timer = setTimeout(() => {
+         // actualFunctionTobeCalled(e);
+         // OR
+         actualFunctionTobeCalled.apply(context, arguments);
+      }, delay);
+
    }
 }
 
-
-/// Function which makes an API call
-// e is always inside callback function of eventlistener
-function apiCaller() {
-   // console.log("API Call Done" + e.target.value );
-   console.log("API Call Done" );
+///? actualFunctionTobeCalled Function which makes an API call
+count = 0;
+function apiCaller(e) {
+   console.log(++count + " API Call Done : " + e.target.value);
+   // console.log(count++ + " API Call Done : ");
 }
-debounceFunc();
-// debounceFunc();
-// debounceFunc();
 
 
 // Q1 ) can you explain value of timeoutId
--- debounce_throttle/throttle.js
+++ debounce_throttle/throttle.js
@@ -1,14 +1,51 @@
+let searchBtn = document.getElementById("searchBtn");
+
+//? without throttling
+// searchBtn.addEventListener("click", apiCaller);
 
 
+//? with throttling
+searchBtn.addEventListener("click", betterApiCaller(apiCaller ,5000));
 
+// ! 1) Namaste JAvascript way of creating throttle (settimeout) 
+function betterApiCaller(actualFunctionTobeCalled, delay) {
+   let flag = true;
+   return function(...args){
+      searchBtn.disabled = true;
+      let context = this;
+      args = arguments;
+      if (flag){
+         actualFunctionTobeCalled.apply(context, arguments);
+         flag = false;
+         setTimeout(() => {
+            actualFunctionTobeCalled.apply(context, arguments);
+            flag = true;
+         }, delay);
+      }
+      
+   }
+}
 
-function throttle(fn , delay) {
+// !2) way of creating throttle (using date time )
+function throttle(actualFunctionTobeCalled, delay) {
    let lastExecution = 0;
-   return function(){
+   return function(...args){
       let now = Date.now();
+      let context = this;
+      args = arguments;
       if(now - lastExecution >= delay){
-         fn();
+         actualFunctionTobeCalled.apply(context, arguments);
          lastExecution = now;
       }
    }
+}
+
+
+
+
+///? actualFunctionTobeCalled Function which makes an API call
+count = 0;
+function apiCaller() {
+   console.log(++count + " API Call Done : ");
+   searchBtn.disabled = false;
 }
