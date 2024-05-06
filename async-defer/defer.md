a5b0183e0d7405335f0a0c5a919872c77751e537
mrinaljain <jain.mrinal140@gmail.com>
2024-05-06 23:44:12 +0530

    Async Defer

---
+++ async-defer/defer.md
@@ -0,0 +1,12 @@
+ Javasrript file load hone k baad kab execute hogi ??
+
+ 
+ NORMAL -->  Jab load karog jaha karog udhar load ho kr  execution start ho jayega
+
+ ASYNC  --> 
+ ye file ko asynchronously load karega aur HTML parallely load hota rahega 
+ jese hi fetch hui vese hi execute ho jayegi 
+
+ DFFER -->  to defer the execution for later time
+
+ script kahi bhi load ho , last mai execute hogi
--- event_propogation/propogation.js
+++ event_propogation/propogation.js
@@ -1,28 +1,46 @@
+/*
+* Event Propogation:
+Event propagation is a mechanism in JavaScript that controls how events are handled in the Document Object Model (DOM) tree. It has below 2 phases
+* Event Capturing (Trickling): 
+   hapens first from   Parent --> Child
+* Event Bubbling:
+happens after caputuring phase  child  -- > Parent
+* stopPropagation :
+Stops furthur journey(Propogation) of the event in DOM
+* stopImmediatePropagation:
+used to stop propogation on similar eventslistners also 
+*/
+
 let grandParent = document.getElementById('grandParent');
 let parent = document.getElementById('parent');
 let child = document.getElementById('child');
 
+const captureGrandParent = false;
+const capturepParent = false;
+const captureChild = false;
+
+const stopGrandParentPropogation = true;
+const stopParentPropogation = true;
+const stopChildPropogation = false;
+
 grandParent.addEventListener('click', function (e) {
    let clickNode = e.target;
    console.log('Grand Parent');
-   // e.stopPropagation();
-}, false);
+
+   if (stopGrandParentPropogation) e.stopPropagation();
+}, captureGrandParent);
+
 parent.addEventListener('click', function (e) {
    let clickNode = e.target;
    console.log('Parent');
-   e.stopPropagation();
-},false);
+   if (stopParentPropogation) e.stopPropagation();
+}, capturepParent);
+
 child.addEventListener('click', function (e) {
    let clickNode = e.target;
    console.log('Child');
-   e.stopPropagation();
-},true);
-// click child
-//child
+   if (stopChildPropogation) e.stopPropagation();
+}, captureChild);
 
 
-
-
-
-// question regarding event propogation  is it possible for diffrent event typey?
-// can click propogate to doubleclick?
+//! Doubt => question regarding event propogation  is it possible for diffrent event type ?  can click propogate to doubleclick?
