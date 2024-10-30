f4472362d44742a1c211914a98e84bad4f266068
mrinaljain <jain.mrinal140@gmail.com>
2024-10-30 14:17:38 +0530

    console methods added

---
+++ C/console/README.md
@@ -0,0 +1,16 @@
+# CONSOLE
+
+console is used to improve the logging  of information inside the web application.
+
+
+
+
+## Connsole Methods
+1. console.log
+2. console.error
+3. console.info
+4. console.warn
+5. console.table
+6. console.group / groupEnd
+7. console.trace
+8. console.time / timeEnd
---
+++ C/console/console.js
@@ -0,0 +1,33 @@
+console.log("Hello world..!");
+
+console.warn("This is a warning");
+
+console.error("This is an error");
+
+console.info("This is some information inside console");
+
+user = {
+  name: "username",
+  lastName: "lastName",
+};
+
+console.table(user);
+
+fruits = ["Orange", "Banana", "Grapes"];
+
+console.trace(fruits);
+
+// Group Logs;
+
+console.group("Console Group");
+console.log("Item : 1");
+console.log("Item : 2");
+console.groupEnd();
+
+console.time("Ended");
+for (var i = 0; i <= 9; i++) {
+  console.timeLog("Ended");
+}
+setTimeout(() => {
+  console.timeEnd("Ended");
+}, 10000);
