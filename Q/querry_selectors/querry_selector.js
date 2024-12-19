d741e09f7bff8d1ca96ecbe0adf713506fdf3b65
mrinaljain <jain.mrinal140@gmail.com>
2024-12-19 23:40:43 +0530

    added notes for querry and tags selector for DOM

---
+++ Q/querry_selectors/querry_selector.js
@@ -0,0 +1,22 @@
+console.log("Testing file connection...");
+
+// var button = document.querySelector("button");
+
+var listItems = document.querySelector("button");
+var buttonsById = document.getElementById("thisId");
+var buttonsByClass = document.getElementsByClassName("thisClass");
+var buttons = document.querySelectorAll("button");
+
+// name attribute will be used below
+var buttonsByName = document.getElementsByName("unique");
+var buttonsByTagName = document.getElementsByTagNameNS("*", "*");
+var buttonsByTag = document.getElementsByTagName("img");
+
+// console.log(listItems);
+// console.log(buttons);
+// console.log(buttonsByName);
+// console.log(buttonsByTagName);
+
+// console.log(buttonsByClass);
+// console.log(buttonsById);
+// console.log(buttonsByTag);
---
+++ Q/querry_selectors/readme.md
@@ -0,0 +1,3 @@
+# Querry Selectors
+
+Javascript DOM manipulation hapens with the help of querry selectors.
