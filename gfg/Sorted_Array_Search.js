148c90a17078745692bbc0b31b5ef86b4e36c785
mrinaljain <jain.mrinal140@gmail.com>
2025-04-08 00:25:23 -0400

    removed dsa questions to seperate repo

--- gfg/Sorted_Array_Search.js
+++
@@ -1,7 +0,0 @@
-/* Given an array, arr[] sorted in ascending order and an integer k. Return true if k is present in the array, otherwise, false.*/
-
-
-let searchInSorted = (arr, k) =>{
-        // your code here
-        return arr.includes(k);
-    }
